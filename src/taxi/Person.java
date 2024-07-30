0328aa6bc8a829fb6b29f2c6ed37d7651667617b
Radmir <Rhody_17@mail.ru>
2024-07-30 19:25:16 +0300

    Please enter the commit message for your changes. Lines starting END

--- src/taxi/Person.java
+++ src/taxi/Person.java
@@ -1,7 +1,37 @@
 package taxi;
 
-public class Person {
-    public void callTaxi(Drivable taxi){
-        taxi.drive("Ленина, 55");
+import java.io.Serializable;
+
+public class Person implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String name;
+    private int age;
+
+    // Конструкторы, геттеры и сеттеры
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Геттеры и сеттеры
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{name='" + name + "', age=" + age + "}";
     }
 }
